8dc65a5b2993c42149e8ef554d1e9ed6d62a734c
KNeegcyao <2903909011@qq.com>
2024-12-11 15:09:14 +0800

    version1

--- src/main/java/com/kneeg/myRPCVersion1/server/RPCServer.java
+++ src/main/java/com/kneeg/myRPCVersion1/server/RPCServer.java
@@ -30,8 +30,21 @@ public class RPCServer {
                         //读取客户端传过来的request
                         RPCRequest request =(RPCRequest)ois.readObject();
                         //反射调用对应方法
+                        //userService.getClass(): 获取 userService 的运行时 Class 对象，表示 userService 的类型信息。
+                        //request.getMethodName(): 从客户端的请求中获取方法名（例如 "findUserById"）。
+                        //request.getParamsTypes(): 获取参数类型数组，用于匹配目标方法的参数签名。
+                        //参数类型是一个 Class[] 数组，例如 [String.class, int.class]。
+                        //这是为了精确定位目标方法（方法重载可能存在多个同名方法）。
+                        //getMethod(String name, Class<?>... parameterTypes): 反射方法，查找类中与指定方法名和参数类型匹配的方法。
                         Method method = userService.getClass()
                                 .getMethod(request.getMethodName(), request.getParamsTypes());
+                        //method.invoke(Object obj, Object... args):
+                        //调用 method 所表示的方法。
+                        //obj: 调用该方法的对象，这里是 userService。
+                        //args: 方法参数的值数组（例如 ["123", 25]）。
+                        //返回值:
+                        //如果方法有返回值，invoke 就是方法的执行结果。
+                        //如果方法返回 void，则 invoke 为 null。
                         Object invoke = method.invoke(userService, request.getParams());
                         //封装，写入reponse对象
                         oos.writeObject(RPCReponse.success(invoke));
